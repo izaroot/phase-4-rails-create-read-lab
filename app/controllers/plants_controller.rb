7c6fe8df1c56bac3bf2b9111893902fe46e130ac
Isaac <izaroot327@gmail.com>
2021-06-07 15:29:23 -0400

    Completed lab

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,21 @@
+class PlantsController < ApplicationController
+    
+    def index
+        plant = Plant.all
+        render json: plant
+    end
+
+    def show
+        plant = Plant.find_by(id: params[:id])
+        if plant
+            render json: plant
+        else
+            render json: {error: "Plant not found"}, status: :not_found
+        end
+    end
+    
+    def create
+        plant = Plant.create(params.permit(:name, :image, :price))
+        render json: plant, status: :created
+    end
+end
